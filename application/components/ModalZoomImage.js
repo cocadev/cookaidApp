53292e0ca03b4b5ef18ba573b6f94ab7e55d435c
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:30:10 +0800

    Update ModalZoomImage.js

--- application/components/ModalZoomImage.js
+++ application/components/ModalZoomImage.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {Modal, StyleSheet,StatusBar, TextInput, Text, View, TouchableHighlight,TouchableOpacity, Dimensions, Image, Button} from "react-native";
+import React, { Component } from 'react'
+import { Modal, StyleSheet, StatusBar, TextInput, Text, View, TouchableHighlight, TouchableOpacity, Dimensions, Image, Button } from "react-native";
 import ImageZoom from 'react-native-image-pan-zoom';
 import ConfigApp from "../utils/ConfigApp";
 import Icono from 'react-native-vector-icons/Ionicons';
@@ -7,11 +7,11 @@ import Icon from 'react-native-vector-icons/Entypo';
 
 const { width, height } = Dimensions.get('window');
 
-class ModalZoomImage extends Component{
+class ModalZoomImage extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { source: { uri: this.props.src } , width : width, height: height * 0.4  };
+    this.state = { source: { uri: this.props.src }, width: width, height: height * 0.4 };
   }
 
   componentWillMount() {
@@ -25,41 +25,41 @@ class ModalZoomImage extends Component{
 
   render() {
 
-    return(
-        <Modal
-            animationType="fade"
-            transparent={true}
-            visible={this.props.visible}
-            onRequestClose={this.props.onClose}>
-          <View style={styles.modalContainer}>
-            <StatusBar backgroundColor={'rgba(0, 0, 0, 0.7)'} />
-            <View style={styles.modalContent}>
-              <View style={[styles.viewClose,{'top' : (height - this.state.height)/2 + 8 }]}>
-                <TouchableOpacity onPress={this.props.onClose}  >
-                  <Icon name="cross" style={styles.closeIcon}/>
-                </TouchableOpacity>
-              </View>
-              <TouchableHighlight onPress={this.props.onClose} style={[styles.viewTop, {'height' : (height - this.state.height)/2 }]}>
-                <View style={[styles.viewTop, {'height' : (height - this.state.height)/2 }]}>
-                </View>
-              </TouchableHighlight>
-              <TouchableHighlight onPress={this.props.onClose}  style={[styles.viewBottom, {'top' : (height - this.state.height)/2 + this.state.height},{'height' : (height - this.state.height)/2}]}>
-                <View style={[styles.viewBottom, {'top' : (height - this.state.height)/2 + this.state.height},{'height' : (height - this.state.height)/2}]}>
-                </View>
-              </TouchableHighlight>
-              <ImageZoom
-                  cropWidth={width}
-                  cropHeight={this.state.height}
-                  imageWidth={this.state.width}
-                  imageHeight={this.state.height}>
-                <Image
-                    style={{ height: this.state.height, width: this.state.width, zIndex: 20}}
-                    source={this.state.source}
-                />
-              </ImageZoom>
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={this.props.visible}
+        onRequestClose={this.props.onClose}>
+        <View style={styles.modalContainer}>
+          <StatusBar backgroundColor={'rgba(0, 0, 0, 0.7)'} />
+          <View style={styles.modalContent}>
+            <View style={[styles.viewClose, { 'top': (height - this.state.height) / 2 + 8 }]}>
+              <TouchableOpacity onPress={this.props.onClose}  >
+                <Icon name="cross" style={styles.closeIcon} />
+              </TouchableOpacity>
             </View>
+            <TouchableHighlight onPress={this.props.onClose} style={[styles.viewTop, { 'height': (height - this.state.height) / 2 }]}>
+              <View style={[styles.viewTop, { 'height': (height - this.state.height) / 2 }]}>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.props.onClose} style={[styles.viewBottom, { 'top': (height - this.state.height) / 2 + this.state.height }, { 'height': (height - this.state.height) / 2 }]}>
+              <View style={[styles.viewBottom, { 'top': (height - this.state.height) / 2 + this.state.height }, { 'height': (height - this.state.height) / 2 }]}>
+              </View>
+            </TouchableHighlight>
+            <ImageZoom
+              cropWidth={width}
+              cropHeight={this.state.height}
+              imageWidth={this.state.width}
+              imageHeight={this.state.height}>
+              <Image
+                style={{ height: this.state.height, width: this.state.width, zIndex: 20 }}
+                source={this.state.source}
+              />
+            </ImageZoom>
           </View>
-        </Modal>
+        </View>
+      </Modal>
     )
   }
 
@@ -72,7 +72,7 @@ let styles = StyleSheet.create({
     backgroundColor: 'transparent'
   },
   modalContent: {
-    width: width ,
+    width: width,
     height: height,
     borderRadius: 0,
     backgroundColor: "rgba(0, 0, 0, 0.5)",
@@ -82,22 +82,22 @@ let styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'relative',
   },
-  viewTop : {
+  viewTop: {
     flex: 1,
     justifyContent: 'center',
     position: 'absolute',
     backgroundColor: "rgba(0, 0, 0, 0.3)",
-    width: width  ,
-    left :0,
-    top : 0,
+    width: width,
+    left: 0,
+    top: 0,
     zIndex: 10,
   },
 
-  viewBottom : {
+  viewBottom: {
     position: 'absolute',
     backgroundColor: "rgba(0, 0, 0, 0.5)",
-    width: width  ,
-    left : 0 ,
+    width: width,
+    left: 0,
     zIndex: 10,
     flex: 1,
     justifyContent: 'center',
@@ -108,10 +108,10 @@ let styles = StyleSheet.create({
     width: 20,
     height: 20,
     backgroundColor: 'transparent',
-    zIndex : 130,
-    alignItems : 'flex-end'
+    zIndex: 130,
+    alignItems: 'flex-end'
   },
-  closeIcon:{
+  closeIcon: {
     fontSize: 20,
     width: 20,
     height: 20,
