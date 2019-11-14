23e73bbaa2496c163366464833adec043bfdf92f
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:30:05 +0800

    Update Marker.js

--- application/components/Marker.js
+++ application/components/Marker.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import {TouchableOpacity, Text, View} from 'react-native'
+import { TouchableOpacity, Text, View } from 'react-native'
 
-
-const Marker = ({onPress, top, left, name}) => (
+const Marker = ({ onPress, top, left, name }) => (
   <TouchableOpacity onPress={onPress}>
-    <View style={{height: 10, width: 10, borderRadius: 5, backgroundColor: 'red', position: 'absolute', top, left}}/>
-    <Text style={{position: 'absolute', top, left}}>
+    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: 'red', position: 'absolute', top, left }} />
+    <Text style={{ position: 'absolute', top, left }}>
       {name}
     </Text>
   </TouchableOpacity>
