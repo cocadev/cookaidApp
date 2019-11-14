b9b53d71911a71c1f9d850035b68e95d638e6a2b
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:30:44 +0800

    Update RestDay.js

--- application/components/RestDay.js
+++ application/components/RestDay.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Image, View, Text} from 'react-native';
-import {StringI18} from '../utils/Strings';
+import React, { Component } from 'react';
+import { Image, View, Text } from 'react-native';
+import { StringI18 } from '../utils/Strings';
 
 class RestDay extends React.Component {
 
@@ -8,10 +8,10 @@ class RestDay extends React.Component {
 
     return (
 
-      <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 80}}>
-        <Image source={require('../../assets/images/restday.png')} style={{width: 150, height: 150, marginBottom: 10}}/>
-        <Text style={{fontSize: 26, fontWeight: 'bold', marginBottom: 8}}>{StringI18.t('ST71')}</Text>
-        <Text style={{fontSize: 18, marginBottom: 8, color: '#b5b5b5'}}>{StringI18.t('ST72')}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 80 }}>
+        <Image source={require('../../assets/images/restday.png')} style={{ width: 150, height: 150, marginBottom: 10 }} />
+        <Text style={{ fontSize: 26, fontWeight: 'bold', marginBottom: 8 }}>{StringI18.t('ST71')}</Text>
+        <Text style={{ fontSize: 18, marginBottom: 8, color: '#b5b5b5' }}>{StringI18.t('ST72')}</Text>
 
       </View>
 
