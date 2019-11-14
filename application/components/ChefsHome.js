8516287c6dc3efce58218536f0a4e549b6482569
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:29:08 +0800

    Update ChefsHome.js

--- application/components/ChefsHome.js
+++ application/components/ChefsHome.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {StackNavigator, withNavigation} from 'react-navigation';
-import {Dimensions, FlatList, TouchableOpacity, View} from 'react-native';
-import {Text} from 'native-base';
+import { StackNavigator, withNavigation } from 'react-navigation';
+import { Dimensions, FlatList, TouchableOpacity, View } from 'react-native';
+import { Text } from 'native-base';
 import ConfigApp from '../utils/ConfigApp';
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import {fetchCuisine} from "../redux/actions/cuisineActions";
-import {cachingCuisineImage, getRandomArray} from "../utils/utils";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { fetchCuisine } from "../redux/actions/cuisineActions";
+import { cachingCuisineImage, getRandomArray } from "../utils/utils";
 import CacheImage from "./CacheImage";
-import {StringI18} from "../utils/Strings";
+import { StringI18 } from "../utils/Strings";
 
 var styles = require('../../assets/files/Styles');
-var {height, width} = Dimensions.get('window');
+var { height, width } = Dimensions.get('window');
 
 class ChefsHome extends React.PureComponent {
 
   RecipesByChef = (chef_id, chef_title, chef_title_original) => {
-    this.props.navigation.navigate('RecipesByChefScreen', {IdChef: chef_id, TitleChef: chef_title, Cuisine: chef_title_original});
+    this.props.navigation.navigate('RecipesByChefScreen', { IdChef: chef_id, TitleChef: chef_title, Cuisine: chef_title_original });
   }
 
   constructor(props) {
@@ -29,7 +29,7 @@ class ChefsHome extends React.PureComponent {
   componentWillMount() {
     if (this.props.cuisines) {
       const cuisines = getRandomArray(this.props.cuisines, 9);
-      this.setState({cuisines})
+      this.setState({ cuisines })
     }
     if (!this.props.cuisines) {
       this.props.fetchCuisine();
@@ -39,7 +39,7 @@ class ChefsHome extends React.PureComponent {
   componentWillReceiveProps(nextProps, nextContext) {
     if (nextProps.cuisines && (!this.state.cuisines || this.state.cuisines.length <= 0)) {
       const cuisines = getRandomArray(nextProps.cuisines, 9);
-      this.setState({cuisines})
+      this.setState({ cuisines })
     }
   }
 
@@ -53,26 +53,26 @@ class ChefsHome extends React.PureComponent {
 
     return (
 
-      <View style={{margin: 5, minHeight: 200}}>
+      <View style={{ margin: 5, minHeight: 200 }}>
         <FlatList
           data={this.state.cuisines}
           refreshing="false"
           numColumns={3}
-          renderItem={({item}) =>
+          renderItem={({ item }) =>
             <TouchableOpacity onPress={this.RecipesByChef.bind(this, item.chef_id, item.chef_title, item.chef_title_original || item.chef_title)} activeOpacity={1}
-                              style={{
-                                flex: 1,
-                                marginHorizontal: 5,
-                                marginBottom: 10,
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                              }}>
+              style={{
+                flex: 1,
+                marginHorizontal: 5,
+                marginBottom: 10,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
               <CacheImage
                 uri={ConfigApp.URL + 'images/' + item.chef_image}
-                style={{height: 80, width: 80, marginBottom: 10, borderRadius: 80 / 2, backgroundColor: 'rgba(0,0,0,0.1)'}}
-                imageStyle={{borderRadius: 80 / 2}}
+                style={{ height: 80, width: 80, marginBottom: 10, borderRadius: 80 / 2, backgroundColor: 'rgba(0,0,0,0.1)' }}
+                imageStyle={{ borderRadius: 80 / 2 }}
               />
-              <Text numberOfLines={1} style={{color: '#9e9e9e', fontSize: 11, marginBottom: 5, textTransform: 'capitalize'}}>{StringI18.t(item.chef_title)}</Text>
+              <Text numberOfLines={1} style={{ color: '#9e9e9e', fontSize: 11, marginBottom: 5, textTransform: 'capitalize' }}>{StringI18.t(item.chef_title)}</Text>
             </TouchableOpacity>
           }
           keyExtractor={(item, index) => index.toString()}
