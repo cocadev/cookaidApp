830f34cc496be98e02f3a7a5e110978321c270a3
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:20:39 +0800

    Tons of multi-translation

---
+++ application/languages/index.js
@@ -0,0 +1,209 @@
+import af from "./af.json"
+import sq from "./sq.json"
+import am from "./am.json"
+import ar from "./ar.json"
+import hy from "./hy.json"
+import az from "./az.json"
+import eu from "./eu.json"
+import be from "./be.json"
+import bn from "./bn.json"
+import bs from "./bs.json"
+import bg from "./bg.json"
+import ca from "./ca.json"
+import ceb from "./ceb.json"
+import zhCN from "./zh-CN.json"
+import zhTW from "./zh-TW.json"
+import co from "./co.json"
+import hr from "./hr.json"
+import cs from "./cs.json"
+import da from "./da.json"
+import nl from "./nl.json"
+import eo from "./eo.json"
+import et from "./et.json"
+import fi from "./fi.json"
+import fr from "./fr.json"
+import fy from "./fy.json"
+import gl from "./gl.json"
+import ka from "./ka.json"
+import de from "./de.json"
+import el from "./el.json"
+import gu from "./gu.json"
+import ht from "./ht.json"
+import ha from "./ha.json"
+import haw from "./haw.json"
+import he from "./he.json"
+import hi from "./hi.json"
+import hmn from "./hmn.json"
+import hu from "./hu.json"
+import is from "./is.json"
+import ig from "./ig.json"
+import id from "./id.json"
+import ga from "./ga.json"
+import it from "./it.json"
+import ja from "./ja.json"
+import jw from "./jw.json"
+import kn from "./kn.json"
+import kk from "./kk.json"
+import km from "./km.json"
+import ko from "./ko.json"
+import ku from "./ku.json"
+import ky from "./ky.json"
+import lo from "./lo.json"
+import la from "./la.json"
+import lv from "./lv.json"
+import lt from "./lt.json"
+import lb from "./lb.json"
+import mk from "./mk.json"
+import mg from "./mg.json"
+import ms from "./ms.json"
+import ml from "./ml.json"
+import mt from "./mt.json"
+import mi from "./mi.json"
+import mr from "./mr.json"
+import mn from "./mn.json"
+import my from "./my.json"
+import ne from "./ne.json"
+import no from "./no.json"
+import ny from "./ny.json"
+import ps from "./ps.json"
+import fa from "./fa.json"
+import pl from "./pl.json"
+import pt from "./pt.json"
+import pa from "./pa.json"
+import ro from "./ro.json"
+import ru from "./ru.json"
+import sm from "./sm.json"
+import gd from "./gd.json"
+import sr from "./sr.json"
+import st from "./st.json"
+import sn from "./sn.json"
+import sd from "./sd.json"
+import si from "./si.json"
+import sk from "./sk.json"
+import sl from "./sl.json"
+import so from "./so.json"
+import es from "./es.json"
+import su from "./su.json"
+import sw from "./sw.json"
+import sv from "./sv.json"
+import tl from "./tl.json"
+import tg from "./tg.json"
+import ta from "./ta.json"
+import te from "./te.json"
+import th from "./th.json"
+import tr from "./tr.json"
+import uk from "./uk.json"
+import ur from "./ur.json"
+import uz from "./uz.json"
+import vi from "./vi.json"
+import cy from "./cy.json"
+import xh from "./xh.json"
+import yi from "./yi.json"
+import yo from "./yo.json"
+import zu from "./zu.json"
+
+export default {
+  af,
+  sq,
+  am,
+  ar,
+  hy,
+  az,
+  eu,
+  be,
+  bn,
+  bs,
+  bg,
+  ca,
+  ceb,
+  "zh-CN": zhCN,
+  "zh-TW": zhTW,
+  co,
+  hr,
+  cs,
+  da,
+  nl,
+  eo,
+  et,
+  fi,
+  fr,
+  fy,
+  gl,
+  ka,
+  de,
+  el,
+  gu,
+  ht,
+  ha,
+  haw,
+  he,
+  hi,
+  hmn,
+  hu,
+  is,
+  ig,
+  id,
+  ga,
+  it,
+  ja,
+  jw,
+  kn,
+  kk,
+  km,
+  ko,
+  ku,
+  ky,
+  lo,
+  la,
+  lv,
+  lt,
+  lb,
+  mk,
+  mg,
+  ms,
+  ml,
+  mt,
+  mi,
+  mr,
+  mn,
+  my,
+  ne,
+  no,
+  ny,
+  ps,
+  fa,
+  pl,
+  pt,
+  pa,
+  ro,
+  ru,
+  sm,
+  gd,
+  sr,
+  st,
+  sn,
+  sd,
+  si,
+  sk,
+  sl,
+  so,
+  es,
+  su,
+  sw,
+  sv,
+  tl,
+  tg,
+  ta,
+  te,
+  th,
+  tr,
+  uk,
+  ur,
+  uz,
+  vi,
+  cy,
+  xh,
+  yi,
+  yo,
+  zu,
+}
