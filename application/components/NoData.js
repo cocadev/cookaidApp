0aa3d5bc65878870388d281a2b7150ff4011e3af
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:30:20 +0800

    Update NoData.js

--- application/components/NoData.js
+++ application/components/NoData.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import { View, Text} from 'react-native';
-import Strings, {StringI18} from '../utils/Strings';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Strings, { StringI18 } from '../utils/Strings';
 
 class NoData extends React.Component {
 
@@ -8,8 +8,8 @@ class NoData extends React.Component {
 
     return (
 
-      <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{fontSize: 14, marginBottom: 8, color: '#D1D1D1'}}>{StringI18.t('ST67')}</Text>
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 14, marginBottom: 8, color: '#D1D1D1' }}>{StringI18.t('ST67')}</Text>
       </View>
 
     )
