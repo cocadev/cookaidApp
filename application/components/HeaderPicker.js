590ca2abf8f876b8ae3323bf8a9de61b696b339e
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:29:25 +0800

    Update HeaderPicker.js

--- application/components/HeaderPicker.js
+++ application/components/HeaderPicker.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Header, Title, Button, Icon, Right, Body, Left} from "native-base";
+import { Header, Title, Button, Icon, Right, Body, Left } from "native-base";
 
 
-const HeaderPicker = ({onPress, title}) => (
-  <Header style={{backgroundColor: "#f39c12"}}>
+const HeaderPicker = ({ onPress, title }) => (
+  <Header style={{ backgroundColor: "#f39c12" }}>
     <Left>
       <Button transparent onPress={onPress}>
-        <Icon name="arrow-back" style={{color: "#fff"}}/>
+        <Icon name="arrow-back" style={{ color: "#fff" }} />
       </Button>
     </Left>
-    <Body style={{flex: 3}}>
-    <Title style={{color: "#fff", fontSize: 20}}>{title}</Title>
+    <Body style={{ flex: 3 }}>
+      <Title style={{ color: "#fff", fontSize: 20 }}>{title}</Title>
     </Body>
-    <Right/>
+    <Right />
   </Header>
 )
 
