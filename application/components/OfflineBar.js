573ac9cfb5e0746081d328a40fbc1b6b00c5e2fe
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-14 23:30:24 +0800

    Update OfflineBar.js

--- application/components/OfflineBar.js
+++ application/components/OfflineBar.js
@@ -1,9 +1,9 @@
-import React, {PureComponent} from 'react';
-import {View, NetInfo, Dimensions, Image} from 'react-native';
-import {Text, Button} from 'native-base';
-import Strings, {StringI18} from '../utils/Strings';
+import React, { PureComponent } from 'react';
+import { View, NetInfo, Dimensions, Image } from 'react-native';
+import { Text, Button } from 'native-base';
+import Strings, { StringI18 } from '../utils/Strings';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 var styles = require('../../assets/files/Styles');
 
 function MiniOfflineSign() {
@@ -20,9 +20,9 @@ function MiniOfflineSign() {
       backgroundColor: '#FFF'
     }}>
       <Image source={require('../../assets/images/nointernet.png')}
-             style={{width: 120, height: 120, marginBottom: 10}}/>
-      <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 8}}>{StringI18.t('ST68')}</Text>
-      <Text style={{fontSize: 16, marginBottom: 30, color: '#b5b5b5'}}>{StringI18.t('ST69')}</Text>
+        style={{ width: 120, height: 120, marginBottom: 10 }} />
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 8 }}>{StringI18.t('ST68')}</Text>
+      <Text style={{ fontSize: 16, marginBottom: 30, color: '#b5b5b5' }}>{StringI18.t('ST69')}</Text>
       <View>
         <Button rounded block style={styles.button_auth}>
           <Text>{StringI18.t('ST70')}</Text>
@@ -39,9 +39,9 @@ class OfflineBar extends PureComponent {
   };
   handleConnectivityChange = isConnected => {
     if (isConnected) {
-      this.setState({isConnected});
+      this.setState({ isConnected });
     } else {
-      this.setState({isConnected});
+      this.setState({ isConnected });
     }
   };
 
@@ -55,7 +55,7 @@ class OfflineBar extends PureComponent {
 
   render() {
     if (!this.state.isConnected) {
-      return <MiniOfflineSign/>;
+      return <MiniOfflineSign />;
     }
     return null;
   }
